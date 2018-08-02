10a12c80dbb48071a1812728a8bec446d592b2ef
Dmitry <dmitry.bagirov94@gmail.com>
2018-08-02 12:24:34 +0600

    added regex to phone

--- app/Http/Requests/BriefRequest.php
+++ app/Http/Requests/BriefRequest.php
@@ -32,8 +32,8 @@ class BriefRequest extends FormRequest
             'files' => '',
             'website' => '',
             'deadline' => '',
-            'email' => 'required',
-            'phone' => '',
+            'email' => 'required|email',
+            'phone' => 'regex:/^\+7\ \(\d{3}\)\ \d{3}\-\d{2}\-\d{2}$/', //^(\+7|[7,8]\ ?)(\d{10}|(\(?\d{3}\)?\ ?\d{3}[\-,\ ]?\d{2}[\-,\ ]?\d{2}))$
             'references' => 'required',
         ];
     }
